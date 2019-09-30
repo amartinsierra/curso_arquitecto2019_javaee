96e5730017da479966510ec0712c51bb04ab11cd
Profesortarde <Profesortarde@G24-PROF>
2019-09-30 19:37:19 +0200

    commit 3 en casa

--- 17_contactos_factory/src/factories/FactoryDao.java
+++ 17_contactos_factory/src/factories/FactoryDao.java
@@ -3,6 +3,7 @@ package factories;
 import daos.DaoContactos;
 import daos.DaoContactosImpl;
 //clase de factoria
+//modificada en casa
 public class FactoryDao {
 	public static DaoContactos getDaoContactos() {
 		return new DaoContactosImpl();
